8d64b2e127938d23847c98f4787f7600b44bd835
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-10 19:02:59 +0200

    Chenge finction

--- sandbox/src/main/java/ua/splinestudio/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/splinestudio/sandbox/MyFirstProgram.java
@@ -3,8 +3,25 @@ package ua.splinestudio.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("World");
+        hello("Oleh");
+        hello("Lesha");
+        double a = 5;
+        double b = 8;
+        System.out.println("Square " + a + " = " + area(a));
+        System.out.println("Square " + a + " = " + area(a,b));
+    }
 
+    public static void hello( String hello){
+        System.out.println("Hello, " + hello + "!");
+    }
+
+    public static double area(double l){
+        return l*l;
+
+    }
+    public static double area(double a, double b){
+        return a * b;
     }
 }
 

184f8b79f4e261138b82b4f3cc62485b35a78310
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-16 16:23:06 +0200

    FillingFormOptimization

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,6 @@ package ua.splinestudio.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ua.splinestudio.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase{
--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ua.splinestudio.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {

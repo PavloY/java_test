35e9b70cbfd53caf0f31dd18bd3595dcaa4209ac
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-18 21:19:43 +0200

    UpdateBeforgeMethod

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupDeletionTests.java
@@ -8,8 +8,8 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    if (app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test", null, null));
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test", "test", "test"));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();

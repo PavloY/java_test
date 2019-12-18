799aeeda26026eaa7f523f3c62cef76968ad1dcb
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-18 17:19:10 +0200

    OptimizeNavigationHelper

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/appmanager/NavigationHelper.java
@@ -10,11 +10,20 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && (driver.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent((By.name("new"))))){
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void initContactCreationPage() { click(By.linkText("add new")); }
 
-    public void gotoHomePage() { click(By.linkText("home")); }
-
+    public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
+        click(By.linkText("home"));
+    }
 }

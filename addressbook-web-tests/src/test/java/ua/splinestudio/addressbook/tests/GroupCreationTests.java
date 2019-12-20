303756af447f53b75939f1e2b63462e04cd4ca75
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-20 11:23:22 +0200

    CheckCountCreationGroup

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ua.splinestudio.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ua.splinestudio.addressbook.model.GroupData;
 
@@ -8,12 +9,10 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test", "test2", "test3"));
-        /*app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test", "test2", "test3"));
-        app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returnToGroupPage();
-        */
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ua.splinestudio.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.splinestudio.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test", "test", "test"));
         }
@@ -16,6 +18,8 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("test", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 
 }

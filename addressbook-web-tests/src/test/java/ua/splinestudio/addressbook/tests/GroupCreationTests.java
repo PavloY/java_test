bdad6340be454608429f4f1e53b5839bde51b834
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-20 14:44:39 +0200

    ChangePrediction

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
@@ -3,17 +3,17 @@ package ua.splinestudio.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ua.splinestudio.addressbook.model.GroupData;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase{
 
     @Test
-    public void testGroupCreation() throws Exception {
+    public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test", "test2", "test3"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
-
 }
 
--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupModificationTests.java
@@ -9,11 +9,11 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test", "test", "test"));
         }
-        app.getGroupHelper().selectGroup();
+        int before = app.getGroupHelper().getGroupCount();
+        app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();

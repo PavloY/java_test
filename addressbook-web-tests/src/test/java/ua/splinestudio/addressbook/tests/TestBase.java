e2da9f0fc994265bbe3a78b887543b1bfdeccbd9
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-21 14:43:04 +0200

    UpdateGroupModificationTests

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/TestBase.java
@@ -1,24 +1,22 @@
 package ua.splinestudio.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ua.splinestudio.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-    @BeforeSuite
-    public void setUp() throws Exception {
-        app.init();
-    }
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
 
-    @AfterSuite
-    public void tearDown() {
-        app.stop();
-    }
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
 
 }

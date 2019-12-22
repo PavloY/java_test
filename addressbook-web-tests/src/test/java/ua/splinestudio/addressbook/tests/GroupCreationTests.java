5c4f8e9f28b426147b510cacb9cd7a791b1813e9
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-22 23:12:20 +0200

    AddedHeshMethod

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
@@ -20,5 +20,16 @@ public class GroupCreationTests extends TestBase{
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
         }
+
+    @Test (enabled = false)
+    public void testBadGroupCreation() {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("test1`~@#$").withFooter("test2").withHeader("test3");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
 }
 

d866b531f04a083e78875672ed4adc1c1602c24f
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-23 23:03:33 +0200

    UpdateContactPhoneTests

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/ContactPhoneTests.java
@@ -1,12 +1,11 @@
 package ua.splinestudio.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 import ua.splinestudio.addressbook.model.ContactData;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase{
 
@@ -16,10 +15,22 @@ public class ContactPhoneTests extends TestBase{
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-    assertThat(contact.getHomePhone(), equalTo(contactInfoFromEditForm.getHomePhone()));
-    assertThat(contact.getMobilePhone(), equalTo(contactInfoFromEditForm.getMobilePhone()));
-    assertThat(contact.getWorkPhone(), equalTo(contactInfoFromEditForm.getWorkPhone()));
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+
   }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) ->! s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll("\\s","").replaceAll("[-()]","");
+    }
+
 }
+
 
 

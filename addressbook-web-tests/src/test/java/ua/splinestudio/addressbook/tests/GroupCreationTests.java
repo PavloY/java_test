03c0f1f7246c88d90cd00def709798fc31fbe81d
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-24 20:25:19 +0200

    JsonGroupCreationTests

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package ua.splinestudio.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -21,20 +23,34 @@ public class GroupCreationTests extends TestBase{
 
     @DataProvider
     public Iterator<Object[]> validGpoups() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
-        String xml = "";
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
         String line = reader.readLine();
         while (line !=null) {
-          xml += line;
+          json += line;
           line = reader.readLine();
         }
-        XStream xstream = new XStream();
-        xstream.processAnnotations(GroupData.class);
-        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json,new TypeToken<List<GroupData>>(){}.getType());
         return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider="validGpoups")
+  @DataProvider
+  public Iterator<Object[]> validGpoupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line !=null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+    @Test(dataProvider="validGpoupsFromJson")
     public void testGroupCreation(GroupData group) {
             app.goTo().groupPage();
             Groups before = app.group().all();

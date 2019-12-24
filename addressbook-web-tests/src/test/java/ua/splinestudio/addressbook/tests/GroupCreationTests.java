184058f9f3ac1dbc1d6e3bb8a3ee02c179a7d934
Pavlo <pavlo.yakovlev@gmail.com>
2019-12-24 17:55:34 +0200

    UpdateGroupCreationTests

--- addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/splinestudio/addressbook/tests/GroupCreationTests.java
@@ -1,25 +1,38 @@
 package ua.splinestudio.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ua.splinestudio.addressbook.model.GroupData;
 import ua.splinestudio.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
 
-    @Test
-    public void testGroupCreation() {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test1").withFooter("test2").withHeader("test3");
-        app.group().create(group);
-        Groups after = app.group().all();
-        assertThat(after.size(), equalTo(before.size() + 1));
-        assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-        }
+    @DataProvider
+    public Iterator<Object[]> validGpoups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider="validGpoups")
+    public void testGroupCreation(GroupData group) {
+            app.goTo().groupPage();
+            Groups before = app.group().all();
+            app.group().create(group);
+            Groups after = app.group().all();
+            assertThat(after.size(), equalTo(before.size() + 1));
+            assertThat(after, equalTo(
+                    before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
 
     @Test (enabled = false)
     public void testBadGroupCreation() {

fcb9170c5866d576499d0162286e734815b801fe
Pavlo <pavlo.yakovlev@gmail.com>
2020-01-28 21:08:15 +0200

    Update tests

--- dev-rentautobus-tests/src/main/java/testpackage/steps/MainSteps.java
+++ dev-rentautobus-tests/src/main/java/testpackage/steps/MainSteps.java
@@ -1,40 +1,51 @@
-package testpackage.steps.serenity;
+package testpackage.steps;
 
 
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Assert;
 import testpackage.pages.MainPage;
 
 public class MainSteps extends ScenarioSteps {
   MainPage page;
 
   @Step
+  @Given("User open main page")
   public void openMainPage() {
   page.open();
   }
 
   @Step
+  @When("User click sign in button")
   public void clickSignUpButton() {
     page.clickSignUpButton();
   }
 
   @Step
+  @When("User set email address \"<emailAddress>\"")
   public void setEmailAddress(String emailAddress) {
     page.setEmailAddress(emailAddress);
   }
 
   @Step
+  @When("User set password field \"<passwordField>\"")
   public void setPasswordField (String passwordField) {
     page.setPasswordField(passwordField);
   }
 
   @Step
+  @When("User click signin button confirmation")
   public void clickSignUpButtonConfirmation() {
     page.clickSignUpButtonConfirmation();
   }
 
   @Step
-  public boolean isUserByTextVisible(String message) {
-    return page.isUserByTextVisible(message);
-  }
+  @Then("User should see \"<message>\"")
+  public void isUserByTextVisible(String message) {
+    Assert.assertTrue(page.isUserByTextVisible(message));}
+
+
 }

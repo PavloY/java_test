b6c63a8a70c0a40b122e4e96f29e3ea9459558ae
Pavlo <pavlo.yakovlev@gmail.com>
2020-01-29 20:56:08 +0200

    Update test

--- dev-rentautobus-tests/src/test/java/testpackage/StepDefenitions/MainPageSteps.java
+++ dev-rentautobus-tests/src/test/java/testpackage/StepDefenitions/MainPageSteps.java
@@ -11,40 +11,34 @@ import testpackage.steps.MainSteps;
 public class MainPageSteps {
 
   @Steps
-  private MainSteps steps;
-
+  MainSteps steps;
 
   @Given("User open main page")
   public void openMainPage() {
     steps.openMainPage();
   }
 
-
   @When("User click sign in button")
   public void clickSignUpButton() {
     steps.clickSignUpButton();
   }
 
-
-  @When("User set email address \"<emailAddress>\"")
+  @When("User set email address '$emailAddress'")
   public void setEmailAddress(String emailAddress) {
     steps.setEmailAddress(emailAddress);
   }
 
-
-  @When("User set password field \"<passwordField>\"")
+  @When("User set password field '$passwordField'")
   public void setPasswordField (String passwordField) {
     steps.setPasswordField(passwordField);
   }
-
 
   @When("User click signin button confirmation")
   public void clickSignUpButtonConfirmation() {
     steps.clickSignUpButtonConfirmation();
   }
 
-
-  @Then("User should see \"<message>\"")
+  @Then("User should see '$message'")
   public void isUserByTextVisible(String message) {
     Assert.assertTrue(steps.isUserByTextVisible(message));}
 
